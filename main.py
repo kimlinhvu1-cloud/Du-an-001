544744749bfc306c49b8bfd5912d1405845503e6
kimlinhvu1-cloud <kimlinhvu1@gmail.com>
2026-02-17 14:09:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello Dung from GitHub!")
+print("=== AI CỦA DŨNG v1 ===")
+
+while True:
+    user = input("Bạn: ")
+
+    if user.lower() == "thoát":
+        print("AI: Tạm biệt Dũng 👋")
+        break
+
+    elif "chào" in user.lower():
+        print("AI: Chào Dũng 😎")
+
+    elif "tên gì" in user.lower():
+        print("AI: Tôi là AI riêng của Dũng.")
+
+    elif "mấy giờ" in user.lower():
+        print("AI: Tôi chưa xem được giờ nhưng sắp nâng cấp 😏")
+
+    else:
+        print("AI: Tôi đang học thêm... Dũng nói lại thử xem.")
